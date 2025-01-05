70ead472610d6b111f96d8dfcd069936d213a1c3
achyut <achyutp10@gmail.com>
2025-01-05 21:05:44 +0545

    initialized for OO

--- OOP/OOP/OperatorOverloading.cs
+++ OOP/OOP/OperatorOverloading.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace OOP
 {
-    internal class OperatorOverloading
+    class Person
     {
+        private string name;
+        private int age;
+
+
+
+        public string Name
+        {
+            get => name;
+
+            set => name = !string.IsNullOrEmpty(value) ? value : "Invalid Name";
+
+        }
+        public int Age
+        {
+            get => age;
+
+            set => age = value >= 0 && value <= 150 ? value : -1;
+
+        }
+
+        public Person(string name, int age)
+        {
+
+            Name = name;
+            Age = age;
+        }
+
+        public string ReturnDetails()
+        {
+            return $"Name: {Name}\n Age: {Age}";
+
+        }
+
+    }
+    public class OperatorOverloading
+    {
+        static void Main22()
+        {
+            Person person = new Person("Achyut", 23);
+        }
     }
 }
--- OOP/OOP/Properties.cs
+++ OOP/OOP/Properties.cs
@@ -1,112 +1,112 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOP
-{
-    class Person
-    {
-        //private string name;
-        //private int age;
-        public string Name { get; set; }
-        public int Age { get; set; }
+//namespace OOP
+//{
+//    class Person
+//    {
+//        //private string name;
+//        //private int age;
+//        public string Name { get; set; }
+//        public int Age { get; set; }
 
-        //public string Name
-        //{
-        //    get
-        //    {
-        //        return name;
-        //    }
-        //    set
-        //    {
-        //        this.name = !string.IsNullOrEmpty(value) ? value : "Invalid Name";
-        //    }
-        //}
-        //public string Name
-        //{
-        //    get => name;
+//        //public string Name
+//        //{
+//        //    get
+//        //    {
+//        //        return name;
+//        //    }
+//        //    set
+//        //    {
+//        //        this.name = !string.IsNullOrEmpty(value) ? value : "Invalid Name";
+//        //    }
+//        //}
+//        //public string Name
+//        //{
+//        //    get => name;
             
-        //    set => name = !string.IsNullOrEmpty(value) ? value : "Invalid Name";
+//        //    set => name = !string.IsNullOrEmpty(value) ? value : "Invalid Name";
             
-        //}
+//        //}
 
-        //public string Name { get => name; set => name = value; }
-        //public int Age
-        //{
-        //    get
-        //    {
-        //        return age;
-        //    }
-        //    set
-        //    {
-        //        age = value;
-        //    }
-        //}
-        //public int Age
-        //{
-        //    get => age;
+//        //public string Name { get => name; set => name = value; }
+//        //public int Age
+//        //{
+//        //    get
+//        //    {
+//        //        return age;
+//        //    }
+//        //    set
+//        //    {
+//        //        age = value;
+//        //    }
+//        //}
+//        //public int Age
+//        //{
+//        //    get => age;
             
-        //    set => age = value >= 0 && value <= 150 ? value : -1;
+//        //    set => age = value >= 0 && value <= 150 ? value : -1;
             
-        //}
-        //public int Age { get => age; set => age = value; }
+//        //}
+//        //public int Age { get => age; set => age = value; }
 
-        public Person(string name, int age)
-        {
-            //this.name = name;
-            //this.age = age;
+//        public Person(string name, int age)
+//        {
+//            //this.name = name;
+//            //this.age = age;
 
-            Name = name;
-            Age = age;
-        }
-
-
-        /*
-        public void SetName(string name)
-        {
-            this.name = !string.IsNullOrEmpty(name) ? name : "Invalid Name";
-        }
-
-        public string GetName()
-        {
-            return name;
-        }
+//            Name = name;
+//            Age = age;
+//        }
 
 
-        public void SetAge(int age)
-        {
-            this.age = age >= 0 && age <= 150 ? age : -1;
-        }
+//        /*
+//        public void SetName(string name)
+//        {
+//            this.name = !string.IsNullOrEmpty(name) ? name : "Invalid Name";
+//        }
 
-        public int GetAge()
-        {
-            return age;
-        }
-        */
+//        public string GetName()
+//        {
+//            return name;
+//        }
 
-        public string ReturnDetails()
-        {
-            return $"Name: {Name}\n Age: {Age}";
 
-        }
+//        public void SetAge(int age)
+//        {
+//            this.age = age >= 0 && age <= 150 ? age : -1;
+//        }
 
-    }
-    public class Properties
-    {
-        public static void Main5()
-        {
-            Person person = new Person("Achyut", 22);
-            Console.WriteLine(person.ReturnDetails());
+//        public int GetAge()
+//        {
+//            return age;
+//        }
+//        */
 
-            person.Name = "Harry";
-            person.Age = 22;
+//        public string ReturnDetails()
+//        {
+//            return $"Name: {Name}\n Age: {Age}";
 
-            //Console.WriteLine(person.ReturnDetails());
+//        }
 
-            Console.WriteLine($"Your name is {person.Name} and age is {person.Age}");
+//    }
+//    public class Properties
+//    {
+//        public static void Main5()
+//        {
+//            Person person = new Person("Achyut", 22);
+//            Console.WriteLine(person.ReturnDetails());
+
+//            person.Name = "Harry";
+//            person.Age = 22;
+
+//            //Console.WriteLine(person.ReturnDetails());
+
+//            Console.WriteLine($"Your name is {person.Name} and age is {person.Age}");
             
-        }
-    }
-}
+//        }
+//    }
+//}
