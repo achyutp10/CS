4dd6527c6574ebdf06e7f9924294a7d467794517
achyut <achyutp10@gmail.com>
2025-01-06 14:02:14 +0545

    Inheritance

--- OOP/OOP/Inheritance.cs
+++ OOP/OOP/Inheritance.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,24 +25,45 @@ namespace OOP
 
     }
 
-    class Student
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
+    class Student : Person
+    {       
         public int Grade { get; set; }
 
-        public Student(string name, int age, int grade)
+        public Student(string name, int age, int grade) : base(name, age)
         {
-            Name = name;
-            Age = age;
             Grade = grade;
         }
 
         public override string ToString()
         {
-            return $"Name: {Name}\tAge: {Age}\tGrade: {Grade}";
+            return $"{base.ToString()}\tGrade: {Grade}";
         }
 
+    }
+    class Staff : Person
+    {
+        public int Salary { get; set; }
+
+        public Staff(string name, int age, int salary) : base(name, age)  
+        { 
+            Salary = salary; 
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}\tSalary: {Salary}";
+        }
+    }
+    class Teacher : Staff
+    {
+        public string Subject { get; set; }
+        public Teacher(string name, int age, int salary, string subject) : base(name, age, salary)
+        {
+            Subject = subject;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}\tSubject: {Subject}";
+        }
     }
     public class Inheritance
     {
@@ -49,8 +71,12 @@ namespace OOP
         {
             Person person = new Person("Achyut", 23);
             Student student = new Student("Achyut", 23, 1);
+            Staff staff = new Staff("Achyut", 23, 10000);
+            Teacher teacher = new Teacher("Achyut", 23, 10000, "C#");
             Console.WriteLine(person);
             Console.WriteLine(student);
+            Console.WriteLine(staff);
+            Console.WriteLine(teacher);
         }
     }
 }
