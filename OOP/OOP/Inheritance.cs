7aa16f5795bea1ad90d3b738fadf3ef610720fdf
achyut <achyutp10@gmail.com>
2025-01-06 13:17:25 +0545

    Inheritance Initialized

---
+++ OOP/OOP/Inheritance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\tAge: {Age}";
+        }
+
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Grade { get; set; }
+
+        public Student(string name, int age, int grade)
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\tAge: {Age}\tGrade: {Grade}";
+        }
+
+    }
+    public class Inheritance
+    {
+        static void Main222()
+        {
+            Person person = new Person("Achyut", 23);
+            Student student = new Student("Achyut", 23, 1);
+            Console.WriteLine(person);
+            Console.WriteLine(student);
+        }
+    }
+}
--- OOP/OOP/OR_ToString.cs
+++ OOP/OOP/OR_ToString.cs
@@ -1,69 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOP
-{
-    class Person
-    {
-        private string name;
-        private int age;
+//namespace OOP
+//{
+//    class Person
+//    {
+//        private string name;
+//        private int age;
 
-        public string Name
-        {
-            get => name;
-            set => name = !string.IsNullOrEmpty(value) ? value : "Invalid name!";
-        }
-        public int Age
-        {
-            get => age;
-            set => age = value >= 0 && value <= 150 ? value : -1;
-        }
-        public Person(string name, int age)
-        {
-            Name = name;
-            Age = age;
-        }
-        public string ReturnDetails()
-        {
-            return $"Name: {name}\nAge: {Age}";
-        }
-        public override string ToString()
-        {
-            return $"Name: {name}\nAge: {Age}";
-        }
-        public override bool Equals(object obj)
-        {
-            if (obj is Person)
-            {
-                Person person = obj as Person; //run time
-                return Name.Equals(person.Name) && Age == person.Age;
-            }
+//        public string Name
+//        {
+//            get => name;
+//            set => name = !string.IsNullOrEmpty(value) ? value : "Invalid name!";
+//        }
+//        public int Age
+//        {
+//            get => age;
+//            set => age = value >= 0 && value <= 150 ? value : -1;
+//        }
+//        public Person(string name, int age)
+//        {
+//            Name = name;
+//            Age = age;
+//        }
+//        public string ReturnDetails()
+//        {
+//            return $"Name: {name}\nAge: {Age}";
+//        }
+//        public override string ToString()
+//        {
+//            return $"Name: {name}\nAge: {Age}";
+//        }
+//        public override bool Equals(object obj)
+//        {
+//            if (obj is Person)
+//            {
+//                Person person = obj as Person; //run time
+//                return Name.Equals(person.Name) && Age == person.Age;
+//            }
 
-            return false;
-        }
-    }
-    class OR_ToString
-    {
-        static void Main(string[] args)
-        {
-            Person person = new Person("Aba", 25);
-            Person test = new Person("Aba", 25);
-            Console.WriteLine(person);
-            Console.WriteLine(test);
+//            return false;
+//        }
+//    }
+//    class OR_ToString
+//    {
+//        static void Main(string[] args)
+//        {
+//            Person person = new Person("Aba", 25);
+//            Person test = new Person("Aba", 25);
+//            Console.WriteLine(person);
+//            Console.WriteLine(test);
 
-            if (person.Equals(test))
-            {
-                Console.WriteLine("Same");
-            }
-            else
-            {
-                Console.WriteLine("Not same");
-            }
+//            if (person.Equals(test))
+//            {
+//                Console.WriteLine("Same");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Not same");
+//            }
 
-            Console.ReadLine();
-        }
-    }
-}
+//            Console.ReadLine();
+//        }
+//    }
+//}
