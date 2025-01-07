6afa94ec9b887f5bce7fdd8d9d2f4b3fef61dcf7
achyut <achyutp10@gmail.com>
2025-01-07 09:48:30 +0545

    static

--- OOP/OOP/Interfacee.cs
+++ OOP/OOP/Interfacee.cs
@@ -1,162 +1,162 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOP
-{
-    public interface IPerson
-    {
-        void DoWork();
+//namespace OOP
+//{
+//    public interface IPerson
+//    {
+//        void DoWork();
 
-        string Name { get; set; }
-        int Age { get; set; }
+//        string Name { get; set; }
+//        int Age { get; set; }
         
-    }
-    abstract class Person : IPerson
-    {
-        //private string name;
-        //private int  age;
+//    }
+//    abstract class Person : IPerson
+//    {
+//        //private string name;
+//        //private int  age;
 
-        //public string Name { 
-        //    get => name; 
-        //    set => name = value; 
-        //}
-        //public int Age { 
-        //    get => age; 
-        //    set => age = value; 
-        //}
+//        //public string Name { 
+//        //    get => name; 
+//        //    set => name = value; 
+//        //}
+//        //public int Age { 
+//        //    get => age; 
+//        //    set => age = value; 
+//        //}
 
-        public string Name { get; set; }
-        public int Age { get; set; }
+//        public string Name { get; set; }
+//        public int Age { get; set; }
 
-        public Person(string name, int age)
-        {
-            Name = name;
-            Age = age;
-        }
+//        public Person(string name, int age)
+//        {
+//            Name = name;
+//            Age = age;
+//        }
 
-        public override string ToString()
-        {
-            return $"Name: {Name}\tAge: {Age}";
-        }
-
-
-        public static bool operator ==(Person a, Person b)
-        {
-            return a.Name.Equals(b.Name) && a.Age == b.Age;
-        }
-        public static bool operator !=(Person a, Person b)
-        {
-            return !a.Name.Equals(b.Name) || a.Age != b.Age;
-
-        }
-
-        public static Person operator ++(Person a)
-        {
-            a.Age++;
-            return a;
-        }
-        public static Person operator --(Person a)
-        {
-            a.Age--;
-            return a;
-        }
-
-        public static bool operator >=(Person a, Person b)
-        {
-            return a.Age >= b.Age;
-        }
-        public static bool operator >(Person a, Person b)
-        {
-            return a.Age > b.Age;
-        }
-        public static bool operator <=(Person a, Person b)
-        {
-            return a.Age <= b.Age;
-        }
-        public static bool operator <(Person a, Person b)
-        {
-            return a.Age < b.Age;
-        }
-        public static Person operator +(Person a, Person b)
-        {
-            a.Age += b.Age;
-            return a;
-        }
-        public abstract void DoWork();
-        //public void DoWork()
-        //{
-        //    Console.WriteLine("Im doing something");
-        //}
-
-    }
-    class Student : Person
-    {
-        public int Grade { get; set; }
-
-        public Student(string name, int age, int grade) : base(name, age)
-        {
-            Grade = grade;
-        }
-
-        public override string ToString()
-        {
-            return $"{base.ToString()}\tGrade: {Grade}";
-        }
-        public override void DoWork()
-        {
-            Console.WriteLine("Im Studing");
-        }
-
-    }
-    class Staff : Person
-    {
-        public int Salary { get; set; }
-
-        public Staff(string name, int age, int salary) : base(name, age)
-        {
-            Salary = salary;
-        }
-        public override string ToString()
-        {
-            return $"{base.ToString()}\tSalary: {Salary}";
-        }
-        public override void DoWork()
-        {
-            Console.WriteLine("I'm Marking papers");
-        }
-    }
-    class Teacher : Staff
-    {
-        public string Subject { get; set; }
-        public Teacher(string name, int age, int salary, string subject) : base(name, age, salary)
-        {
-            Subject = subject;
-        }
-        public override string ToString()
-        {
-            return $"{base.ToString()}\tSubject: {Subject}";
-        }
-
-        public override void DoWork()
-        {
-            Console.WriteLine("Im Teaching");
-        }
+//        public override string ToString()
+//        {
+//            return $"Name: {Name}\tAge: {Age}";
+//        }
 
 
-    }
-    public class Interfacee
-    {
-        public static void Main222()
-        {
-            Student student = new Student("Achyut", 23, 1);
-            Staff staff = new Staff("Achyut", 23, 10000);
-            Teacher teacher = new Teacher("Achyut", 23, 10000, "C#");
-            student.DoWork();
-            staff.DoWork();
-            teacher.DoWork();
-        }
-    }
-}
+//        public static bool operator ==(Person a, Person b)
+//        {
+//            return a.Name.Equals(b.Name) && a.Age == b.Age;
+//        }
+//        public static bool operator !=(Person a, Person b)
+//        {
+//            return !a.Name.Equals(b.Name) || a.Age != b.Age;
+
+//        }
+
+//        public static Person operator ++(Person a)
+//        {
+//            a.Age++;
+//            return a;
+//        }
+//        public static Person operator --(Person a)
+//        {
+//            a.Age--;
+//            return a;
+//        }
+
+//        public static bool operator >=(Person a, Person b)
+//        {
+//            return a.Age >= b.Age;
+//        }
+//        public static bool operator >(Person a, Person b)
+//        {
+//            return a.Age > b.Age;
+//        }
+//        public static bool operator <=(Person a, Person b)
+//        {
+//            return a.Age <= b.Age;
+//        }
+//        public static bool operator <(Person a, Person b)
+//        {
+//            return a.Age < b.Age;
+//        }
+//        public static Person operator +(Person a, Person b)
+//        {
+//            a.Age += b.Age;
+//            return a;
+//        }
+//        public abstract void DoWork();
+//        //public void DoWork()
+//        //{
+//        //    Console.WriteLine("Im doing something");
+//        //}
+
+//    }
+//    class Student : Person
+//    {
+//        public int Grade { get; set; }
+
+//        public Student(string name, int age, int grade) : base(name, age)
+//        {
+//            Grade = grade;
+//        }
+
+//        public override string ToString()
+//        {
+//            return $"{base.ToString()}\tGrade: {Grade}";
+//        }
+//        public override void DoWork()
+//        {
+//            Console.WriteLine("Im Studing");
+//        }
+
+//    }
+//    class Staff : Person
+//    {
+//        public int Salary { get; set; }
+
+//        public Staff(string name, int age, int salary) : base(name, age)
+//        {
+//            Salary = salary;
+//        }
+//        public override string ToString()
+//        {
+//            return $"{base.ToString()}\tSalary: {Salary}";
+//        }
+//        public override void DoWork()
+//        {
+//            Console.WriteLine("I'm Marking papers");
+//        }
+//    }
+//    class Teacher : Staff
+//    {
+//        public string Subject { get; set; }
+//        public Teacher(string name, int age, int salary, string subject) : base(name, age, salary)
+//        {
+//            Subject = subject;
+//        }
+//        public override string ToString()
+//        {
+//            return $"{base.ToString()}\tSubject: {Subject}";
+//        }
+
+//        public override void DoWork()
+//        {
+//            Console.WriteLine("Im Teaching");
+//        }
+
+
+//    }
+//    public class Interfacee
+//    {
+//        public static void Main222()
+//        {
+//            Student student = new Student("Achyut", 23, 1);
+//            Staff staff = new Staff("Achyut", 23, 10000);
+//            Teacher teacher = new Teacher("Achyut", 23, 10000, "C#");
+//            student.DoWork();
+//            staff.DoWork();
+//            teacher.DoWork();
+//        }
+//    }
+//}
---
+++ OOP/OOP/StaticFunctions.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    interface IPerson
+    {
+        void DoWork();
+        string Name { get; set; }
+        int Age { get; set; }
+    }
+    abstract class Person : IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public static bool operator ==(Person a, Person b)
+        {
+            return a.Name.Equals(b.Name) && a.Age == b.Age;
+        }
+        public static bool operator !=(Person a, Person b)
+        {
+            return !a.Name.Equals(b.Name) || a.Age != b.Age;
+        }
+        public static Person operator ++(Person a)
+        {
+            a.Age++;
+            return a;
+        }
+        public static Person operator --(Person a)
+        {
+            a.Age--;
+            return a;
+        }
+        public static bool operator >(Person a, Person b)
+        {
+            return a.Age > b.Age;
+        }
+        public static bool operator >=(Person a, Person b)
+        {
+            return a.Age >= b.Age;
+        }
+        public static bool operator <(Person a, Person b)
+        {
+            return a.Age < b.Age;
+        }
+        public static bool operator <=(Person a, Person b)
+        {
+            return a.Age <= b.Age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\tAge: {Age}";
+        }
+
+        public abstract void DoWork();
+    }
+    class Student : Person
+    {
+        public int Grade { get; set; }
+
+        public Student(string name, int age, int grade) : base(name, age)
+        {
+            Grade = grade;
+        }
+        public override void DoWork()
+        {
+            Console.WriteLine("I'm studying!");
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}\tGrade: {Grade}";
+        }
+        public static Student CreateStudent()
+        {
+            Console.Write("Enter student name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter student age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter student grade: ");
+            int grade = Convert.ToInt32(Console.ReadLine());
+
+            return new Student(name, age, grade);
+        }
+    }
+
+    class Staff : Person
+    {
+        public int Salary { get; set; }
+
+        public Staff(string name, int age, int salary) : base(name, age)
+        {
+            Salary = salary;
+        }
+        public override void DoWork()
+        {
+            Console.WriteLine("I'm marking papers.");
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}\tSalary: {Salary}";
+        }
+        public static Staff CreateStaff(bool teacher = false)
+        {
+            string context = teacher ? "teacher" : "staff";
+
+            Console.Write($"Enter {context} name: ");
+            string name = Console.ReadLine();
+
+            Console.Write($"Enter {context} age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Enter {context} salary: ");
+            int salary = Convert.ToInt32(Console.ReadLine());
+
+            return new Staff(name, age, salary);
+        }
+    }
+
+    class Teacher : Staff
+    {
+        public string Subject { get; set; }
+
+        public Teacher(string name, int age, int salary, string subject) : base(name, age, salary)
+        {
+            Subject = subject;
+        }
+        public Teacher(Staff staff, string subject) : base(staff.Name, staff.Age, staff.Salary)
+        {
+            Subject = subject;
+        }
+        public override void DoWork()
+        {
+            Console.WriteLine("I'm teaching");
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}\tSubject: {Subject}";
+        }
+        public static Teacher CreateTeacher()
+        {
+            Staff staff = Staff.CreateStaff(true);
+
+            Console.Write("Enter teacher subject: ");
+            string subject = Console.ReadLine();
+
+            //return new Teacher(staff.Name, staff.Age, staff.Salary, subject);
+            return new Teacher(staff, subject);
+        }
+    }
+    class StaticFunctions
+    {
+        static void Main()
+        {
+            Student student = Student.CreateStudent();
+            Staff staff = Staff.CreateStaff();
+            Teacher teacher = Teacher.CreateTeacher();
+
+            Console.ReadLine();
+        }
+    }
+}
+
