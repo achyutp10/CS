ccc09fec54c7e66a3a9db742457f4b1d8c4df2cd
achyut <achyutp10@gmail.com>
2025-01-07 16:07:37 +0545

    sealed

---
+++ OOP/OOP/Sealedd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    abstract class A
+    {
+        public abstract void s();
+        public abstract void a();
+    }
+    class B : A
+    {
+        public sealed override void a() { }
+        public override void s() { }
+        public void b()
+        {
+
+        }
+    }
+    class C : B
+    {
+        //public override void a() { }
+        public override void s()
+        {
+            b();
+        }
+    }
+    class Sealedd
+    {
+        static void Mainn()
+        {
+            Console.ReadLine();
+        }
+    }
+}
